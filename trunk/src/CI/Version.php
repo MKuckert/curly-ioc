8dbbd7dc68333ffcf634cc2b4158bb527e4ad84c
martin@curlybracket.de <martin@curlybracket.de@6c150d98-9a40-11de-a465-5fc2cdd6756b>
2009-09-05 17:27:38 +0000

    Added Version.php

---
+++ trunk/src/CI/Version.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * CI_Version
+ * 
+ * @author Martin Kuckert
+ * @copyright Copyright (c) 2009 Martin Kuckert
+ * @license New BSD License
+ * @package CI
+ * @since 05.09.2009
+ */
+class CI_Version {
+	
+	const VERSION='0.1alpha';
+	
+}
